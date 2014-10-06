fd8957e75c258e277e23fd029743c3490bd1d3ba
Michał Budzyński <michal@virtualdesign.pl>
2014-10-06 01:01:00 +0100

    [NPMCO.DE-SITE] - :doughnut: home module update

--- site/home/home.js
+++ site/home/home.js
@@ -1,13 +1,9 @@
 steal(
   'can',
   function(can) {
-    var ViewModel = can.Map.extend({});
-
-    console.log('elo');
     return can.Component.extend({
       tag: 'home-app',
-      template: can.view('./site/home/home.mustache'),
-      scope: ViewModel
+      template: can.view('./site/home/home.mustache')
     });
   }
 );

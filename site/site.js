36e07c6087cbd9eb4c0662afed7cf22f3eac51e8
Michał Budzyński <michal@virtualdesign.pl>
2014-10-08 01:01:00 +0100

    [NPMCO.DE-SITE] - :books: module path changed

--- site/site.js
+++ site/site.js
@@ -2,7 +2,7 @@ steal(
 	'can',
 	'./home/home.js',
 	'./snippet-list/snippet-list.js',
-	'./fixtures/fixtures.js',
+	'./fixtures.js',
 	function(can, home, snippetList) {
 		can.$('#app').html(can.view('./site/home/home.mustache', {
 			snippets: new snippetList.List({})

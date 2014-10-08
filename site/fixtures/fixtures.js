aa014757d8e37f18547940e046513687fa096388
Michał Budzyński <michal@virtualdesign.pl>
2014-10-08 01:01:00 +0100

    [NPMCO.DE-SITE] - :tongue: example fixtures added

--- site/fixtures/fixtures.js
+++ site/fixtures/fixtures.js
@@ -1,6 +1,27 @@
 steal(
   'can',
   function(can) {
-    
+    return can.fixture({
+      "POST /services/players":  function(request, response) {
+        response([
+          {
+            "package": "groovebasin",
+            "filename": "lib/web_socket_api_client.js",
+            "score": 0.29015115,
+            "content": [
+              "(<em>JSON.stringify</em>({\n      name: name,\n      args: args,\n    }));\n  } catch (err) {\n    // nothing to do"
+            ]
+          },
+          {
+            "package": "sequelize-cli",
+            "filename": "test/url.test.js",
+            "score": 0.27371186,
+            "content": [
+              "(<em>JSON.stringify</em>(helpers.getTestConfig()), \"config/config.json\"))\n      .pipe(helpers.runCli(\"db:migrate \" + flag"
+            ]
+          }
+        ]);
+      }
+    });
   }
 );

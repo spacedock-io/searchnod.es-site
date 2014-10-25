818c328d9a8a449672060e6b6352c6149b94f426
Michał Budzyński <michal@virtualdesign.pl>
2014-10-25 01:01:00 +0100

    :palm_tree: can-click to can-submit

--- site/home/home.js
+++ site/home/home.js
@@ -1,17 +1,17 @@
 steal(
   'can',
   function(can) {
-    console.log("QWEQWEQWEQWEa");
     return can.Component.extend({
       tag: 'home-app',
       template: can.view('./site/home/home.mustache'),
+      scope: {
+        sbmt: function(){
+          console.log('sbmt actn');
+        }
+      },
       events: {
-        inserted: function(){
-          console.log('eldoka?');
-        },
         'form submit': function(el, ev) {
           // Don't submit the form
-          console.log('elo!');
           ev.preventDefault();
           return false;
         }

d020c12627cd269576a867fa4e8c5dddba6b59c2
Michał Budzyński <michal@virtualdesign.pl>
2014-10-25 01:01:00 +0100

    :space_invader: home module improvements

--- site/home/home.js
+++ site/home/home.js
@@ -1,9 +1,21 @@
 steal(
   'can',
   function(can) {
+    console.log("QWEQWEQWEQWEa");
     return can.Component.extend({
       tag: 'home-app',
-      template: can.view('./site/home/home.mustache')
+      template: can.view('./site/home/home.mustache'),
+      events: {
+        inserted: function(){
+          console.log('eldoka?');
+        },
+        'form submit': function(el, ev) {
+          // Don't submit the form
+          console.log('elo!');
+          ev.preventDefault();
+          return false;
+        }
+      }
     });
   }
 );

c6c78c81a1c2ea82db03f4ca70af1379189eeb40
Michał Budzyński <michal@virtualdesign.pl>
2014-10-25 01:01:00 +0100

    :person_with_blond_hair: home module events

--- site/home/home.js
+++ site/home/home.js
@@ -1,9 +1,19 @@
 steal(
   'can',
   function(can) {
+    var ViewModel = can.Map.extend({
+      elo: function() {
+        console.log("EDJKSNA");
+      }
+    });
+
     return can.Component.extend({
       tag: 'home-app',
-      template: can.view('./site/home/home.mustache')
+      template: can.view('./site/home/home.mustache'),
+      scope: ViewModel,
+      inserted: function() {
+        console.log('ins');
+      }
     });
   }
 );

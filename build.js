7acd36d53b1a917ac9efad4492f92eba1684998b
Michał Budzyński <michal@virtualdesign.pl>
2014-11-02 01:01:00 +0100

    :ant: remove unneded flags form build file

--- build.js
+++ build.js
@@ -4,12 +4,5 @@ stealTools.build({
   main: "site/site",
   config: __dirname+"/stealconfig.js"
 },{
-  bundleSteal: true,
-  // the following are the default values, so you don't need
-  // to write them.
-  minify: true,
-  debug: false,
-  quiet: false,
-  bundleDepth: 3,
-  mainDepth: 3
+  bundleSteal: true
 })

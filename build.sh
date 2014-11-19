e517c63611e0c4d933615564729d37100a57a13a
Michał Budzyński <michal@virtualdesign.pl>
2014-11-19 01:03:57 +0100

    temporary build script created

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+node build.js
+if [[ $? == 0 ]]; then
+   cp -R site/components/rainbow dist/rainbow
+   cp -R img dist/img
+   cp -R style dist/style
+   cp index.production.html dist
+   cp index.production.html dist
+fi

09fadeccc90efb639981465c2d240bb46fe03fd4
Michał Budzyński <michal@virtualdesign.pl>
2014-11-25 01:01:00 +0100

    :department_store: page buttons component

---
+++ site/snippet-list/page_buttons.js
@@ -0,0 +1,17 @@
+steal(
+  'can',
+  './page_buttons.mustache!',
+  function(can, pbTemplate) {
+    var pages = new can.List([1]);
+
+    can.Component.extend({
+      tag: 'page-buttons',
+      template: pbTemplate,
+      scope: {
+        pages: pages
+      }
+    });
+
+    return pages;
+  }
+);

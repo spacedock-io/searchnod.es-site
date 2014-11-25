4ce3d50252b498ad15fc88ad3deb2c841813f98e
Michał Budzyński <michal@virtualdesign.pl>
2014-11-25 01:01:00 +0100

    :lollipop: change fixtures

--- site/fixtures.js
+++ site/fixtures.js
@@ -3,150 +3,34 @@ steal(
   function(fixture) {
     var fixObject = {};
     fixObject["POST " + steal.config('apiLocation') + "/search?q={searchTerm}"] = function(request, response) {
-      response([
-        {
-          "package": "groovebasin",
-          "filename": "lib/web_socket_api_client.js",
-          "score": 0.29015115,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify({\n      name: name,\n      args: args,\n    }));\n  } catch (err) {\n    // nothing to do"
-          ]
-        },
-        {
-          "package": "sequelize-cli",
-          "filename": "test/url.test.js",
-          "score": 0.27371186,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify(helpers.getTestConfig()), \"config/config.json\"))\n      .pipe(helpers.runCli(\"db:migrate \" + flag"
-          ]
-        },
-        {
-          "package": "groovebasin",
-          "filename": "lib/web_socket_api_client.js",
-          "score": 0.29015115,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify({\n      name: name,\n      args: args,\n    }));\n  } catch (err) {\n    // nothing to do"
-          ]
-        },
-        {
-          "package": "sequelize-cli",
-          "filename": "test/url.test.js",
-          "score": 0.27371186,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify(helpers.getTestConfig()), \"config/config.json\"))\n      .pipe(helpers.runCli(\"db:migrate \" + flag"
-          ]
-        },
-        {
-          "package": "groovebasin",
-          "filename": "lib/web_socket_api_client.js",
-          "score": 0.29015115,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify({\n      name: name,\n      args: args,\n    }));\n  } catch (err) {\n    // nothing to do"
-          ]
-        },
-        {
-          "package": "sequelize-cli",
-          "filename": "test/url.test.js",
-          "score": 0.27371186,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify(helpers.getTestConfig()), \"config/config.json\"))\n      .pipe(helpers.runCli(\"db:migrate \" + flag"
-          ]
-        },
-        {
-          "package": "groovebasin",
-          "filename": "lib/web_socket_api_client.js",
-          "score": 0.29015115,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify({\n      name: name,\n      args: args,\n    }));\n  } catch (err) {\n    // nothing to do"
-          ]
-        },
-        {
-          "package": "sequelize-cli",
-          "filename": "test/url.test.js",
-          "score": 0.27371186,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify(helpers.getTestConfig()), \"config/config.json\"))\n      .pipe(helpers.runCli(\"db:migrate \" + flag"
-          ]
-        },
-        {
-          "package": "groovebasin",
-          "filename": "lib/web_socket_api_client.js",
-          "score": 0.29015115,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify({\n      name: name,\n      args: args,\n    }));\n  } catch (err) {\n    // nothing to do"
-          ]
-        },
-        {
-          "package": "sequelize-cli",
-          "filename": "test/url.test.js",
-          "score": 0.27371186,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify(helpers.getTestConfig()), \"config/config.json\"))\n      .pipe(helpers.runCli(\"db:migrate \" + flag"
-          ]
-        },
-        {
-          "package": "groovebasin",
-          "filename": "lib/web_socket_api_client.js",
-          "score": 0.29015115,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify({\n      name: name,\n      args: args,\n    }));\n  } catch (err) {\n    // nothing to do"
-          ]
-        },
-        {
-          "package": "sequelize-cli",
-          "filename": "test/url.test.js",
-          "score": 0.27371186,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify(helpers.getTestConfig()), \"config/config.json\"))\n      .pipe(helpers.runCli(\"db:migrate \" + flag"
-          ]
-        },
-        {
-          "package": "groovebasin",
-          "filename": "lib/web_socket_api_client.js",
-          "score": 0.29015115,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify({\n      name: name,\n      args: args,\n    }));\n  } catch (err) {\n    // nothing to do"
-          ]
-        },
-        {
-          "package": "sequelize-cli",
-          "filename": "test/url.test.js",
-          "score": 0.27371186,
-          "author" : "test author",
-          "avatar" : "/avatar.jpg",
-          "content": [
-            "(JSON.stringify(helpers.getTestConfig()), \"config/config.json\"))\n      .pipe(helpers.runCli(\"db:migrate \" + flag"
+      response({
+        "hits": {
+          "total": 228,
+          "hits": [
+            {
+              "package": "npm",
+              "filename": "lib/faq.js",
+              "author": "othiym23",
+              "score": 1.2665461,
+              "content": [
+                "faq.usage = \"npm faq\"\n\nvar npm = require(\"./npm.js\")\n\nfunction faq (args, cb) { npm.commands.help([\"faq\"], cb) }"
+              ]
+            },
+            {
+              "package": "npm",
+              "filename": "lib/get.js",
+              "author": "othiym23",
+              "score": 1.1688331,
+              "content": [
+                "module.exports = get\n\nget.usage = \"npm get <key> <value> (See `npm config`)\"\n\nvar npm = require(\"./npm.js\")\n\nget.completion = npm.commands.config.completion",
+                "get.completion = npm.commands.config.completion\n\nfunction get (args, cb) {\n  npm.commands.config([\"get\"].concat(args), cb)"
+              ]
+            }
           ]
         }
-      ]);
+      });
     };
-    
+
     return fixture(fixObject);
   }
 );
